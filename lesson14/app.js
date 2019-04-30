c6b1e06bb8f2586910d5a7909e8d52e358b8a217
volosgoto <volosgoto@yahoo.com>
2019-04-30 10:02:25 +0300

    event modifiers

---
+++ lesson14/app.js
@@ -0,0 +1,12 @@
+new Vue({
+  el: "#app",
+  data: {
+    title: "Conditional rendering if",
+    name: "John"
+  },
+  methods: {
+    logMessage() {
+      console.log("Log message");
+    }
+  }
+});
