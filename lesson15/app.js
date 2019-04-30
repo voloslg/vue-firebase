9b8e6a00f681b03391e2b68a66b9e9032cbe35f3
volosgoto <volosgoto@yahoo.com>
2019-04-30 10:43:06 +0300

    for

--- lesson15/app.js
+++ lesson15/app.js
@@ -2,15 +2,12 @@ new Vue({
   el: "#app",
   data: {
     title: "For loop",
-    showName: false,
-    showAge: true
+    items: ["Apple", "Potato", "Orange", "Beer", "Coka Cola"],
+    ninjas: [
+      { name: "Crystal", age: 25, belt: "Black" },
+      { name: "Wind", age: 18, belt: "Gerren" },
+      { name: "Sub Zero", age: 40, belt: "Brown" }
+    ]
   },
-  methods: {
-    toggleName() {
-      this.showName = !this.showName;
-    },
-    toggleAge() {
-      this.showAge = !this.showAge;
-    }
-  }
+  methods: {}
 });
